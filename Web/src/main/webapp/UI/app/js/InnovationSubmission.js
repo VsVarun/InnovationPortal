5556da06661f577418622090d757423b9bd2df62
sumandevan <suman.devan@yahoo.com>
2014-12-10 20:13:28 +0530

    added the validation for the innovationsubmission page
    added the validation for the innovationsubmission page

--- Web/src/main/webapp/UI/app/js/InnovationSubmission.js
+++ Web/src/main/webapp/UI/app/js/InnovationSubmission.js
@@ -3,17 +3,28 @@ jQuery(document).ready(function($){
 	loadCategoryList();
 	loadTeamList();
 	
-	$("#IdeaSubmission").submit(function(event){
-		postJSon("../../../Services/Idea/Submit", buildPostData(), function (response) {
-			if(response.statusCode == 200){
-				swal("Idea Submitted!!!", "Thanks for your Valuable Submission...", "success");	
-			}else{
-				swal("Idea Submission Failed!!!", response.statusDesc, "error");
-			}
-	    }, null);
-		
-		// Cancel the actual Submission
-		event.preventDefault();
+    $("#IdeaSubmission").submit(function (event) {
+      
+        var validationstatus = innovationsubValidation();
+      // alert(innovationsubValidation());
+
+        if (validationstatus == true) {
+          // alert('posted');
+           postJSon("../../../Services/Idea/Submit", buildPostData(), function (response) {
+               if (response.statusCode == 200) {
+                   swal("Idea Submitted!!!", "Thanks for your Valuable Submission...", "success");
+               } else {
+                   swal("Idea Submission Failed!!!", response.statusDesc, "error");
+               }
+           }, null);
+       }
+
+       else {
+
+           // Cancel the actual Submission
+           event.preventDefault();
+       }
+
 	});
 	
 });	
@@ -44,4 +55,47 @@ function buildPostData(){
 		lastModifiedDate : $.now()
 	};
 	return JSON.stringify(idea);
+}
+
+
+function innovationsubValidation() {
+    var validationstatus = true;
+    var missingfields = '';
+    var missingfieldcount = 0;
+   
+
+    if($("#ideaSummary").val()=='') {
+        validationstatus = false;
+        missingfields = 'Summary' + ', ';
+        missingfieldcount = 1;
+    }
+    if ($("#ideaDescription").val() == '') {
+        validationstatus = false;
+        missingfields = missingfields + 'Description' + ', ';
+        missingfieldcount = missingfieldcount + 1;
+    }
+    if ($("#ideaBenefits").val() == '') {
+        validationstatus = false;
+        missingfields = missingfields  + 'Benefits' + ', ';
+        missingfieldcount = missingfieldcount + 1;
+    }
+    if ($("#ideaChallenges").val() == '') {
+        validationstatus = false;
+        missingfields = missingfields + 'Challenges' + ', ';
+        missingfieldcount = missingfieldcount + 1;
+    }
+   
+    //alert(Boolean(validationstatus));
+    //alert(parseInt(missingfieldcount));
+
+    if (Boolean(validationstatus) == false) {
+        if (parseInt(missingfieldcount) == 1) {
+            swal(missingfields.replace(',','') + " field is missing!!!", "Please enter missing fields...", "warning");
+        }
+        else {
+            swal(missingfields + " field's are missing!!!", "Please enter missing fields...", "warning");
+        }
+    }
+   
+     return Boolean(validationstatus);
 }
